3088b60a6e1be92861850ed54eebbb22688d5b04
S.V. Paulauskas <stanpaulauskas@gmail.com>
2014-06-10 07:46:51 -0400

    Fixing some botched commits and adding ignore file
    Signed-off-by: S.V. Paulauskas <stanpaulauskas@gmail.com>

--- src/TraceFilter.cpp
+++ src/TraceFilter.cpp
@@ -109,22 +109,12 @@ void TraceFilter::CalcEnergyFilter(void) {
 
 void TraceFilter::CalcEnergyFilterCoeffs(void) {
     double l = e_.GetRisetime();
-    //coeffs according to H.T.
-    // double beta = exp(-1.0/ e_.GetTau());
-    // double cg = 1-beta;
-    // double ctmp = 1-pow(beta,e_.GetRisetime());
-    // coeffs_.push_back(-(cg/ctmp)*pow(beta,e_.GetRisetime()));
-    // coeffs_.push_back(cg);
-    // coeffs_.push_back(cg/ctmp);
-
-    //Coeffs according to C.P.
-    // double beta = exp(-1./e_.GetTau());
-    // double a0 = pow(beta,l)/(pow(beta,l)-1.0);
-    // double ag = 1.0;
-    // double a1 = -1.0/(pow(beta,l)-1.0);
-    // coeffs_.push_back(a0);
-    // coeffs_.push_back(ag);
-    // coeffs_.push_back(a1);
+    double beta = exp(-1.0/ e_.GetTau());
+    double cg = 1-beta;
+    double ctmp = 1-pow(beta,e_.GetRisetime());
+    coeffs_.push_back(-(cg/ctmp)*pow(beta,e_.GetRisetime()));
+    coeffs_.push_back(cg);
+    coeffs_.push_back(cg/ctmp);
 
     if(loud_)
         cout << "The Energy Filter Coefficients: " << endl
